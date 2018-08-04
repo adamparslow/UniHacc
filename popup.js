7df3df2220fef5f9fe805c163c8b994ffd0b4394
z5122506 <z5122506@student.unsw.edu.au>
2018-08-04 17:43:01 +1000

    Javascript for plusMenu dynamic length

--- popup.js
+++ popup.js
@@ -16,3 +16,23 @@ function openPlusMenu () {
     var plusmenu = document.getElementById("plusMenu");
     plusmenu.style.display = "block";
 }
+
+
+var fields = 0;
+function addFields() {
+   fields++;
+   var plusForm = document.getElementById('plusForm');
+   var nameTxt = document.createTextNode("Name: ");
+   plusForm.appendChild(nameTxt);
+   var name = document.createElement('input');
+   name.type = "text";
+   name.id = "name" + fields;
+   plusForm.appendChild(name);
+   var urlTxt = document.createTextNode("URL: ");
+   plusForm.appendChild(urlTxt);
+   var url = document.createElement('input');
+   url.type = "text";
+   url.id = "url" + fields;
+   plusForm.appendChild(url);
+   plusForm.appendChild('br');
+}
