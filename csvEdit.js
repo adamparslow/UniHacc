418f6a052ac3f53abc121f6d4bef1b3e2fd858eb
Adam T <ataras2@gmail.com>
2018-08-04 20:16:31 +1000

    CSV read/write data

---
+++ csvEdit.js
@@ -0,0 +1,19 @@
+
+function addGroup(groupName, args) {
+	var data  = [];
+	n = arguments.length;
+	// set up array [[alias1,URL1],[alias2,URL2]...]
+	for (i = 1; i < n; i += 2){
+		data.push([arguments[i], arguments[i+1]])
+	}
+	console.log(data);
+	chrome.storage.sync.set({groupName: data}, {
+          console.log('Value is set to ' + value);
+        });
+}	
+
+function getData(groupName){
+	chrome.storage.sync.get(groupName, function(items){
+          console.log(items);
+        });
+}
